148235e81ffba597a4137d16dfe29e4850038100
mylesmurphy3207 <112994560+mylesmurphy3207@users.noreply.github.com>
2022-09-28 22:17:35 -0700

    9/28/22

---
+++ test.js
@@ -0,0 +1,28 @@
+// console.log("Hello")
+// var myfirstvar = "Myles";
+// var age=25;
+// let = var
+let age = "25 years old";
+let username = "myles";
+let password = "murphy";
+let like = "like post";
+let save = "save post";
+let share = "share post";
+let searchBar = "search";
+let instagramLogo = "homepage"
+let notification = "notifications"
+let followers = "people who follow you"
+let foryouPage = "algorthm selected pages";
+let peopleYouMayKnow = "mutual friends";
+let following = "people you follow";
+let edit = "edit profile";
+let live = "people who are live";
+let videohover = "autoplay";
+let refresh = "refresh items on page"
+let settings = "your settings";
+let login = "enter your login";
+let  logoff = "enter your logoff";
+
+document.write("<h3> Name: " + username + password + "</h3>"); 
+
+//console.log("My name is:" + username + password);
